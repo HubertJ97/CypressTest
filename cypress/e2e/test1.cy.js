9b537870d375128e75ab869449df08a834fef2c3
HubertJ97 <84916127+HubertJ97@users.noreply.github.com>
2022-12-05 20:49:18 +0100

    Test

--- cypress/e2e/test1.cy.js
+++ cypress/e2e/test1.cy.js
@@ -10,6 +10,7 @@ it('google test', () => {
     cy.get('#L2AGLb').click()
     cy.get('.gLFyf').type('Automation step by step{enter}')
     cy.get('input.gLFyf').should('be.visible')
+    cy.get('.dmenKe > .cIkxbf > .usJj9c > h3 > .l').click()
     
 
 })
